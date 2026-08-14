57d2cf4e8b109366e0c0d01c5748adcd1b2c061e
Prakhar Sharma <prakharsharma8077@gmail.com>
2026-08-14 10:30:55 +0530

    Time: 2 ms (99.82%) | Memory: 48.5 MB (67.26%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int n = nums.length;
+        int j = 1;
+
+        for (int i = 0; i < n; i += 2) {
+            if (nums[i] % 2 != 0) {
+                while (nums[j] % 2 != 0) {
+                    j += 2;
+                }
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        return nums;
+    }
+}
